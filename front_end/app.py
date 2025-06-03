7614d49e9b1fc4baab8ad69e6c60a782f2a6b2e1
Neeraj <153256352+neerajprao@users.noreply.github.com>
2025-06-03 19:42:48 +0530

    Update app.py

--- front_end/app.py
+++ front_end/app.py
@@ -313,4 +313,6 @@ def predict():
 
 if __name__ == '__main__':
     load_models()
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
